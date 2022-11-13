f05887183a51e0b6b3cb51ec2b4f85fa9e1bf5fa
LuzTovar <lx16169@innovaccion.mx>
2022-11-13 16:28:27 -0600

    Sitio de web

---
+++ README.md
@@ -0,0 +1,10 @@
+# Módulo 3 - CSS.
+
+Durante el transcuro de convertirse en un Full Stack Developer, se ha cursado por "Módulo: Diseño y Estructura de ideas", "Módulo 2: HTML", y el "Módulo 3: CSS" en donde al final del contenido se realiza la siguiente práctica que es una página de vacunacion, apartir de un diseño previo realizarla lo más acercado posible
+
+## ! VACUNATE ¡
+## SI TODOS NOS VACUNAMOS CORREMOS MENOS RIESGO.
+Es importante que te apliques tu o tus dosis correpondientes, que no se te pasen las fechas.Hazlo por ti, por tu familia y por todos.
+Si necesitas mas información puedes visitar esta [página](./index.html)
+
+#![Vacuna](./img/descarga2.jpg)
---
+++ eventos.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    var x = document.getElementById('BDiv');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+    }
+    function myFunction2() {
+    var x = document.getElementById('BDiv2');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+    }
+    function myFunction3() {
+    var x = document.getElementById('BDiv3');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+    }
+    function myFunction4() {
+    var x = document.getElementById('BDiv4');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+    }
+    function myFunction5() {
+    var x = document.getElementById('BDiv5');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+    }
